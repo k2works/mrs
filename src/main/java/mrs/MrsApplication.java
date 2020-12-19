bc39063458339ff1166672a59f8974153fde5ae7
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-12-19 21:17:06 +0900

    initial setup

---
+++ src/main/java/mrs/MrsApplication.java
@@ -0,0 +1,13 @@
+package mrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mrs/MrsApplicationTests.java
@@ -0,0 +1,13 @@
+package mrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
