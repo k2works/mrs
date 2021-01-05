c26c9b7256687365c1d9dc53660d3a9eb33c209d
k2works <kakimomokuri@gmail.com>
2021-01-05 16:50:30 +0900

    test: 認証サービス

---
+++ src/test/java/mrs/MrsTestConfiguration.java
@@ -0,0 +1,31 @@
+package mrs;
+
+import mrs.domain.model.RoleName;
+import mrs.domain.model.User;
+import mrs.domain.service.user.ReservationUserDetails;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@TestConfiguration
+public class MrsTestConfiguration {
+    @Bean
+    @Primary
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+                User user = new User();
+                user.setUserId(s);
+                user.setFirstName("太郎");
+                user.setLastName("山田");
+                user.setRoleName(RoleName.ADMIN);
+                user.setPassword("password");
+                return new ReservationUserDetails(user);
+            }
+        };
+    }
+}
---
+++ src/test/java/mrs/domain/service/user/ReservationUserDetailsServiceTest.java
@@ -0,0 +1,54 @@
+package mrs.domain.service.user;
+
+import mrs.MrsTestConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = MrsTestConfiguration.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationUserDetailsServiceTest {
+   @Autowired
+   ReservationUserDetailsService reservationUserDetailsService;
+   @Autowired
+   UserDetailsService userDetailsService;
+
+   @Test
+   @Sql("/schema.sql")
+   @Sql("/data.sql")
+   public void ユーザー情報を取得する() {
+      UserDetails result = reservationUserDetailsService.loadUserByUsername("taro-yamada");
+
+      assertEquals("taro-yamada", result.getUsername());
+   }
+
+   @Test
+   @WithUserDetails("test")
+   public void フィクスチャーを使ってユーザー情報を取得する() {
+      UserDetails result = userDetailsService.loadUserByUsername("test");
+
+      assertEquals("test", result.getUsername());
+      assertEquals("password", result.getPassword());
+   }
+
+   @Test
+   @WithMockUser(username = "test2", roles = {"USER", "ADMIN"})
+   public void モックを使って認証情報を取得する() {
+      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+      assertEquals("test2", auth.getName());
+      assertEquals("password", auth.getCredentials());
+      assertEquals("ROLE_ADMIN", auth.getAuthorities().toArray()[0].toString());
+      assertEquals("ROLE_USER", auth.getAuthorities().toArray()[1].toString());
+   }
+}
