012b34ee3da0edc4322157b780f93a44197e5a4e
k2works <kakimomokuri@gmail.com>
2021-01-07 13:07:58 +0900

    refactor: テストケースのインライン化

--- src/test/java/mrs/application/service/room/RoomServiceTest.java
+++ src/test/java/mrs/application/service/room/RoomServiceTest.java
@@ -2,28 +2,49 @@ package mrs.application.service.room;
 
 import mrs.MrsApplication;
 import mrs.domain.model.reservation.ReservableRoom;
+import mrs.domain.model.room.MeetingRoom;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 @SpringBootTest(classes = MrsApplication.class)
+@DisplayName("会議室サービス")
 public class RoomServiceTest {
-    @Autowired
-    RoomService roomService;
+    @Nested
+    @DisplayName("会議室一覧")
+    class ListRooms {
+        @Autowired
+        RoomService roomService;
 
-    @Test
-    @Sql("/schema.sql")
-    @Sql("/data.sql")
-    public void 会議室一覧を取得する() {
-        LocalDate date = LocalDate.now();
-        List<ReservableRoom> rooms = roomService.findReservableRooms(date);
+        @Test
+        @Sql("/schema.sql")
+        @Sql("/data.sql")
+        @Transactional
+        public void 会議室一覧を取得する() {
+            MeetingRoom rooms = roomService.findMeetingRoom(1);
 
-        assertEquals(2, rooms.size());
+            assertEquals(1, rooms.roomId());
+        }
+
+        @Test
+        @Sql("/schema.sql")
+        @Sql("/data.sql")
+        public void 予約可能会議室一覧を取得する() {
+            LocalDate date = LocalDate.now();
+            List<ReservableRoom> rooms = roomService.findReservableRooms(date);
+
+            assertEquals(2, rooms.size());
+        }
     }
 }
+
