cf64beb424aa036add04f232f85090c22581a721
k2works <kakimomokuri@gmail.com>
2021-01-07 16:33:14 +0900

    feat: 日付タイプ追加

---
+++ src/main/java/mrs/domain/type/date/CurrentDate.java
@@ -0,0 +1,23 @@
+package mrs.domain.type.date;
+
+import java.time.LocalDate;
+
+/**
+ * 現在日
+ * (状態の時点を表現するクラス)
+ */
+public class CurrentDate {
+    LocalDate value;
+
+    public CurrentDate(LocalDate value) {
+        this.value = value;
+    }
+
+    public static CurrentDate parse(String dateText) {
+        return new CurrentDate(LocalDate.parse(dateText));
+    }
+
+    public LocalDate value() {
+        return value;
+    }
+}
