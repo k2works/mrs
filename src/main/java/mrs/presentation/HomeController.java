2e6f4c82dd5130b6803d367b2d2afcee8ae0bdde
k2works <kakimomokuri@gmail.com>
2021-01-07 13:47:02 +0900

    feat: ルートアクセスは会議室一覧にリダイレクトする

---
+++ src/main/java/mrs/presentation/HomeController.java
@@ -0,0 +1,12 @@
+package mrs.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    String home() {
+        return "redirect:/rooms";
+    }
+}
