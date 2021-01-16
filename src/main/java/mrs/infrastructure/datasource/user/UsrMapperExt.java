5b2fbf7836070a22484cb16bec6d2d9ace76e519
k2works <kakimomokuri@gmail.com>
2021-01-16 18:10:21 +0900

    refactor: MyBatis移行

--- src/main/java/mrs/infrastructure/datasource/user/UsrMapperExt.java
+++ src/main/java/mrs/infrastructure/datasource/user/UsrMapperExt.java
@@ -1,6 +1,8 @@
 package mrs.infrastructure.datasource.user;
 
 import mrs.infrastructure.datasource.UsrMapper;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UsrMapperExt extends UsrMapper {
 }
--- src/test/java/mrs/infrastructure/datasource/user/UsrMapperExtTest.java
+++ src/test/java/mrs/infrastructure/datasource/user/UsrMapperExtTest.java
@@ -1,17 +1,30 @@
 package mrs.infrastructure.datasource.user;
 
+import mrs.domain.model.Usr;
+import mrs.domain.model.user.RoleName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 @SpringBootTest
 public class UsrMapperExtTest {
+    @Autowired
+    UsrMapperExt usrMapper;
+
     @Test
     void ユーザーを登録する() {
+        Usr user = new Usr();
+        user.setUserId("user");
+        user.setFirstName("山田");
+        user.setLastName("太郎");
+        user.setPassword("パスワード");
+        user.setRoleName(RoleName.USER.toString());
+        usrMapper.insert(user);
 
-    }
+        Usr result = usrMapper.selectByPrimaryKey("user");
 
-    @Test
-    void ユーザーを検索する() {
-
+        assertNotNull(result);
     }
 }
