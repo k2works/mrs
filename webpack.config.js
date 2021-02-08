4eb300b1123958177f673d275b6653733356c2a7
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-02-08 19:12:38 +0900

    chore: Webpackセットアップ

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/main/resources/static/js/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "public"),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/main/resources/templates/index.html",
+            filename: "index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: { url: false },
+                    },
+                ],
+            },
+        ],
+    },
+};
