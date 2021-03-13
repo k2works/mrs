cff4efb6897c03388d784d718b2494c909fbf655
k2works <kakimomokuri@gmail.com>
2021-03-13 16:39:56 +0900

    chore: セキュリティ設定の分割

--- src/main/java/mrs/WebSecurityConfig.java
+++ src/main/java/mrs/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package mrs;
 import mrs.application.service.user.ReservationUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,56 +13,59 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final ReservationUserDetailsService userDetailsService;
+public class WebSecurityConfig {
+    @Configuration
+    @Order(1)
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public static class UiWebSecurityConfig extends WebSecurityConfigurerAdapter {
+        private final ReservationUserDetailsService userDetailsService;
 
-    public WebSecurityConfig(ReservationUserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+        public UiWebSecurityConfig(ReservationUserDetailsService userDetailsService) {
+            this.userDetailsService = userDetailsService;
+        }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs",
-                "/swagger-resources/configuration/ui",
-                "/swagger-resources",
-                "/swagger-resources/configuration/security",
-                "/swagger-ui.html");
-    }
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers("/v2/api-docs",
+                    "/swagger-resources/configuration/ui",
+                    "/swagger-resources",
+                    "/swagger-resources/configuration/security",
+                    "/swagger-ui.html");
+        }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                // 認可
-                .antMatchers("/js/**", "/css/**").permitAll()
-                .antMatchers("/console/**").permitAll()
-                .antMatchers("/swagger-ui.html").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                // 認証
-                .formLogin()
-                .loginPage("/loginForm")
-                .loginProcessingUrl("/login")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .defaultSuccessUrl("/", true)
-                .failureUrl("/loginForm?error=true").permitAll()
-                // For H2 Console
-                .and()
-                .headers().frameOptions().disable()
-                .and()
-                .csrf().disable();
-    }
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    // 認可
+                    .antMatchers("/js/**", "/css/**").permitAll()
+                    .antMatchers("/console/**").permitAll()
+                    .antMatchers("/swagger-ui.html").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    // 認証
+                    .formLogin()
+                    .loginPage("/loginForm")
+                    .loginProcessingUrl("/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/", true)
+                    .failureUrl("/loginForm?error=true").permitAll()
+                    // For H2 Console
+                    .and()
+                    .headers().frameOptions().disable()
+                    .and()
+                    .csrf().disable();
+        }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        }
     }
 }
