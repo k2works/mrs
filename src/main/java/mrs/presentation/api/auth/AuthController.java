341807170fcf02d313ab2b0530abb5c09c4eb0b1
k2works <kakimomokuri@gmail.com>
2021-04-19 22:30:19 +0900

    refactor: クラスの抽出

--- src/main/java/mrs/presentation/api/auth/AuthController.java
+++ src/main/java/mrs/presentation/api/auth/AuthController.java
@@ -1,83 +1,63 @@
 package mrs.presentation.api.auth;
 
-import mrs.application.repository.UserRepository;
-import mrs.application.service.user.ReservationUserDetails;
-import mrs.domain.model.user.*;
+import mrs.application.service.auth.AuthService;
+import mrs.domain.model.user.Name;
+import mrs.domain.model.user.Password;
+import mrs.domain.model.user.User;
+import mrs.domain.model.user.UserId;
 import mrs.infrastructure.payload.request.LoginRequest;
 import mrs.infrastructure.payload.request.SignupRequest;
 import mrs.infrastructure.payload.response.JwtResponse;
 import mrs.infrastructure.payload.response.MessageResponse;
-import mrs.infrastructure.security.jwt.JwtUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController("JWT認証API")
 @RequestMapping("/api/auth")
 public class AuthController {
-    @Autowired
     AuthenticationManager authenticationManager;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
     PasswordEncoder encoder;
+    AuthService authService;
 
-    @Autowired
-    JwtUtils jwtUtils;
+    public AuthController(AuthenticationManager authenticationManager, PasswordEncoder encoder, AuthService authService) {
+        this.authenticationManager = authenticationManager;
+        this.encoder = encoder;
+        this.authService = authService;
+    }
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
         );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
+        JwtResponse jwtResponse = authService.createJwtResponse(authentication);
 
-        ReservationUserDetails userDetails = (ReservationUserDetails) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
-        User user = userDetails.getUser();
-
-        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(), roles, user));
+        return ResponseEntity.ok(jwtResponse);
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
-        Optional<User> user = Optional.ofNullable(userRepository.findById(signupRequest.getUserId()));
+        Optional<User> user = authService.findUserById(signupRequest.getUserId());
         if (user.isPresent()) {
             return ResponseEntity
                     .badRequest()
                     .body(new MessageResponse("Error: Username is already taken!"));
         }
 
-        //Create new user's account
         UserId userId = new UserId(signupRequest.getUserId());
         Password password = new Password(encoder.encode(signupRequest.getPassword()));
         Name name = new Name(signupRequest.getFirstName(), signupRequest.getLastName());
         String role = signupRequest.getRole();
-        RoleName roleName;
-        if (role == null) {
-            roleName = RoleName.USER;
-        } else {
-            roleName = RoleName.valueOf(role);
-        }
-        User newUser = new User(userId, password, name, roleName);
-        userRepository.save(newUser);
+
+        authService.createUser(userId, password, name, role);
 
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
