2cee2f7e26021fcbb76827b78bff2656e2e65f40
k2works <kakimomokuri@gmail.com>
2021-04-22 18:39:50 +0900

    feat: デザインの変更

--- src/main/typescript/components/top/TopComponent.tsx
+++ src/main/typescript/components/top/TopComponent.tsx
@@ -9,17 +9,41 @@ type Props = {
 const TopComponent: React.FC<Props> = (props: Props) => {
     const history = useHistory();
 
+    // @ts-ignore
     return (
         <div>
             <main>
-                <h2>会議室予約システムへようこそ {props.user.name.firstName} {props.user.name.lastName} さん</h2>
-
-                <p>このシステムは、利用者がインターネット経由で予約できる画期的なシステムです</p>
-
-                <h3>お知らせ</h3>
-                <dl>
-                    <dt>会議室予約機能をリリースしました</dt>
-                </dl>
+                <header className="jumbotron subhead" id="overview">
+                    <div className="row">
+                        <div className="span6">
+                            <h1>
+                                <blink>
+                                    <font color="#00ffB4">会</font>
+                                    <font color="#00ffff">議</font>
+                                    <font color="#00B4ff">室</font>
+                                    <font color="#005Aff">予</font>
+                                    <font color="#0000ff">約</font>
+                                    <font color="#4B00ff">シ</font>
+                                    <font color="#A500ff">ス</font>
+                                    <font color="#FF00ff">テ</font>
+                                    <font color="#FF00B4">ム</font>
+                                </blink>
+                            </h1>
+                            <p className="lead">ようこそ {props.user.name.firstName} {props.user.name.lastName} さん</p>
+                        </div>
+                        <div className="span6">
+                        </div>
+                    </div>
+                    <marquee>このシステムは、利用者がインターネット経由で予約できる画期的なシステムです</marquee>
+                    <div className="subnav">
+                        <ul className="nav nav-pills">
+                            <h3>お知らせ</h3>
+                            <dl>
+                                <dt>会議室予約機能をリリースしました</dt>
+                            </dl>
+                        </ul>
+                    </div>
+                </header>
             </main>
         </div>
 );
--- types/import-react-validation.ts
+++ types/import-react-validation.ts
@@ -1,3 +1,10 @@
 declare module 'react-validation/build/form'
 declare module 'react-validation/build/input'
 declare module 'react-validation/build/button'
+declare namespace JSX {
+    interface IntrinsicElements {
+        marquee: any
+        blink: any
+        font: any
+    }
+}
