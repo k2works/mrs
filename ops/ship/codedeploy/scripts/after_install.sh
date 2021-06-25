de223024d0565ac5f71562bfbf4a91e3569d21cc
k2works <kakimomokkuri@gmail.com>
2021-06-25 18:25:20 +0900

    chore: CodeDeploy更新

--- ops/ship/codedeploy/scripts/after_install.sh
+++ ops/ship/codedeploy/scripts/after_install.sh
@@ -2,6 +2,7 @@
 . $(dirname $0)/common_variables.sh
 
 cp /srv/mrs/nginx/https.conf /etc/nginx/conf.d
+cp /srv/mrs/nginx/http.conf /etc/nginx/conf.d
 cp /srv/mrs/nginx/cert.crt /etc/nginx
 cp /srv/mrs/nginx/cert.key /etc/nginx
 
